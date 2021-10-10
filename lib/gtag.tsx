b82426e8fdc5d9a7f036680b588c09473849dc74
proshunsuke <shunsuke0901@gmail.com>
2021-10-10 14:51:21 +0900

    GAタグを使用する

---
+++ lib/gtag.tsx
@@ -0,0 +1,89 @@
+// thanks for: https://panda-program.com/posts/nextjs-google-analytics
+import { useRouter } from 'next/router';
+import Script from 'next/script';
+import { useEffect } from 'react';
+
+export const GA_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID || '';
+
+// IDが取得できない場合を想定する
+export const existsGaId = GA_ID !== '';
+
+// PVを測定する
+export const pageview = (path: string) => {
+  window.gtag('config', GA_ID, {
+    page_path: path
+  });
+};
+
+// GAイベントを発火させる
+export const event = ({ action, category, label, value = '' }: Event) => {
+  if (!existsGaId) {
+    return;
+  }
+
+  window.gtag('event', action, {
+    event_category: category,
+    event_label: label ? JSON.stringify(label) : '',
+    value
+  });
+};
+
+// _app.tsx で読み込む
+export const usePageView = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!existsGaId) {
+      return;
+    }
+
+    const handleRouteChange = (path: string) => {
+      pageview(path);
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+};
+
+// _app.tsx で読み込む
+export const GoogleAnalytics = () => (
+  <>
+    {existsGaId && (
+      <>
+        <Script defer src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`} strategy='afterInteractive' />
+        <Script
+          id='gtag-script'
+          defer
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());    
+              gtag('config', '${GA_ID}');
+            `
+          }}
+          strategy='afterInteractive'
+        />
+      </>
+    )}
+  </>
+);
+
+// イベントを型で管理
+type ContactEvent = {
+  action: 'submit_form'
+  category: 'contact'
+}
+
+type ClickEvent = {
+  action: 'click'
+  category: 'other'
+}
+
+export type Event = (ContactEvent | ClickEvent) & {
+  label?: Record<string, string | number | boolean>
+  value?: string
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,14 @@ import type { AppProps } from 'next/app';
 import { MDXProvider } from '@mdx-js/react';
 import MDXComponents from '../components/MDXComponents';
 import { ThemeProvider } from 'next-themes';
+import { GoogleAnalytics, usePageView } from '../lib/gtag';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  usePageView();
   return (
     <ThemeProvider attribute="class">
       <MDXProvider components={MDXComponents}>
+        <GoogleAnalytics />
         <Component {...pageProps} />
       </MDXProvider>
     </ThemeProvider>
