d5f8ef972d37ed323f581e62be8156b7ad276506
proshunsuke <proshunsuke@users.noreply.github.com>
2021-12-22 07:24:01 +0000

    deploy: 807bc41bcd3ad43af1e8b2041ccd953414cfad63

---
+++ _next/static/5kw1JflV0DfMDxpeUNGYX/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fposts","\u002Fabout-page","\u002Fposts\u002Ftest-post-1","\u002Fposts\u002Ftest-post-2","\u002Fposts\u002Ftest-post-3","\u002Fresume"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
--- _next/static/aYc5xVNFS0C8q8becJlmu/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fabout-page","\u002Fposts","\u002Fposts\u002Ftest-post-1","\u002Fposts\u002Ftest-post-2","\u002Fposts\u002Ftest-post-3","\u002Fresume"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
