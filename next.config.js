ba28051f73c45c5c9e2d8256a5040799137e48b2
proshunsuke <shunsuke0901@gmail.com>
2021-12-22 11:21:54 +0900

    next-compose-plugins導入

--- next.config.js
+++ next.config.js
@@ -15,3 +15,12 @@ const withMDX = require('@next/mdx')({
 module.exports = withMDX({
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx']
 });
+
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
+
+module.exports = withPlugins([
+  [optimizedImages, {
+    /* config for next-optimized-images */
+  }],
+]);
