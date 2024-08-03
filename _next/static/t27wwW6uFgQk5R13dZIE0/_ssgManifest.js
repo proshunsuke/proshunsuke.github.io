772b35095196558c2eba7b2135f2a5a67e1e1d81
proshunsuke <proshunsuke@users.noreply.github.com>
2024-08-03 03:11:20 +0000

    deploy: cf3cf311d67034dd42357c9389c2c57c430c5b08

--- _next/static/t27wwW6uFgQk5R13dZIE0/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fposts","\u002Fabout-page","\u002Fposts\u002Ftest-post-1","\u002Fposts\u002Ftest-post-3","\u002Fposts\u002Fgithub-copy-title-link","\u002Fresume","\u002Fposts\u002Ftest-post-2"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
