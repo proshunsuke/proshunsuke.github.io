2efc720776da627ad9f6ec4e9183f7ea4b6f4ed1
proshunsuke <shunsuke0901@gmail.com>
2026-09-17 10:56:18 +0900

    開発サーバー起動時にブラウザーを自動で開く

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { reactRouter } from '@react-router/dev/vite';
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'vite';
-import { fileURLToPath } from 'node:url';
+import { reactRouter } from "@react-router/dev/vite";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import { fileURLToPath } from "node:url";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter()],
-  resolve: { alias: { '~': fileURLToPath(new URL('./app', import.meta.url)) } },
+  server: { open: true },
+  resolve: { alias: { "~": fileURLToPath(new URL("./app", import.meta.url)) } },
 });
